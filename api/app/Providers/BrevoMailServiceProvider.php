d9ba7b0690c6cd516ce9f359daffaf9d40f0d039
kevin <kevinkokodoko@gmail.com>
2026-07-15 19:28:29 +0000

    BrevoMailServiceProvider ne s'active que si MAIL_MAILER=brevo

--- api/app/Providers/BrevoMailServiceProvider.php
+++ api/app/Providers/BrevoMailServiceProvider.php
@@ -4,16 +4,20 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Mail;
-use Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoTransportFactory;
-use Symfony\Component\Mailer\Transport\Dsn;
 
 class BrevoMailServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        // N'enregistrer le transport Brevo que si c'est le mailer actif
+        // Évite l'erreur AbstractHttpTransport quand MAIL_MAILER != brevo
+        if (config('mail.default') !== 'brevo') {
+            return;
+        }
+
         Mail::extend('brevo', function () {
-            return (new BrevoTransportFactory)->create(
-                new Dsn(
+            return (new \Symfony\Component\Mailer\Bridge\Brevo\Transport\BrevoTransportFactory)->create(
+                new \Symfony\Component\Mailer\Transport\Dsn(
                     'brevo+api',
                     'default',
                     config('services.brevo.key'),
